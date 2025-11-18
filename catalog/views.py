1acbce34e0eedbf30d0cc3bb824fdac01a9cfe59
gr436_alale <nastya.nastya07012@gmail.com>
2025-11-18 19:11:18 +0700

    доделала 6

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,6 @@
+import authors
 from django.shortcuts import render
+from django.views import generic
 
 # Create your views here.
 from .models import Book, Author, BookInstance, Genre
@@ -29,3 +31,26 @@ class BookListView(generic.ListView):
 class BookDetailView(generic.DetailView):
     model = Book
     paginate_by = 10
+
+class AuthorsListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+def index(request):
+    ...
+
+    num_authors=Author.objects.count()  # The 'all()' is implied by default.
+
+    # Number of visits to this view, as counted in the session variable.
+    num_visits=request.session.get('num_visits', 0)
+    request.session['num_visits'] = num_visits+1
+
+    # Render the HTML template index.html with the data in the context variable.
+    return render(
+        request,
+        'index.html',
+        context={'num_books':num_books,'num_instances':num_instances,'num_instances_available':num_instances_available,'num_authors':num_authors,
+            'num_visits':num_visits}, # num_visits appended
+    )
--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -8,6 +8,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('catalog/', include('catalog.urls')),
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
-]
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
